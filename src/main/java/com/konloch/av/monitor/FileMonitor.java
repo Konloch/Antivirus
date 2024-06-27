efa9735d6fdd7c3b296d1dfc7be7c342565865cb
Konloch <konloch@gmail.com>
2024-06-27 04:16:36 -0600

    Ideas

---
+++ src/main/java/com/konloch/av/monitor/FileMonitor.java
@@ -0,0 +1,11 @@
+package com.konloch.av.monitor;
+
+/**
+ * @author Konloch
+ * @since 6/25/2024
+ */
+public class FileMonitor
+{
+	//TODO watch the file system for changes made to files
+	// we could lock on file create but then we would lack the ability to scan for signature / etc
+}
---
+++ src/main/java/com/konloch/av/monitor/ProcessMonitor.java
@@ -0,0 +1,11 @@
+package com.konloch.av.monitor;
+
+/**
+ * @author Konloch
+ * @since 6/25/2024
+ */
+public class ProcessMonitor
+{
+	//TODO watch processes for non-approved processes, lock them and wait until scan verified it's a non threat
+	// alternatively we can enforce a strict whitelist only system where you need to manually approve the process in the GUI
+}
