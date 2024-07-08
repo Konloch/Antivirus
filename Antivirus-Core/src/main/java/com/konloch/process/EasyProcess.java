a6e0749fd1ed4a17a31744a958bc412b2375a848
Konloch <konloch@gmail.com>
2024-07-08 04:43:17 -0600

    Prevent Resource Exhaust

--- Antivirus-Core/src/main/java/com/konloch/process/EasyProcess.java
+++ Antivirus-Core/src/main/java/com/konloch/process/EasyProcess.java
@@ -67,6 +67,15 @@ public class EasyProcess
 				{
 					e.printStackTrace();
 				}
+				
+				try
+				{
+					Thread.sleep(1);
+				}
+				catch (InterruptedException e)
+				{
+					e.printStackTrace();
+				}
 			}
 		});
 		
